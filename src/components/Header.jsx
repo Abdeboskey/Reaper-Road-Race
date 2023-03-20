2710c1b94b2e62b52f97ddea898665b50bcab907
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-20 10:16:20 -0400

    Change navlink names and add pages alias

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import CtaButton from 'components/CtaButton'
 import logo from 'assets/white_pepper.svg'
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false)
-  const linkStyle = "block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-300 mr-6"
+  const linkStyle = "block mt-4 lg:inline-block lg:mt-0 text-2xl tracking-wide text-white hover:text-green-300 mr-6"
 
   return (
     <header className="sticky top-0 font-heading z-50">
       <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-red-500 to-red-800 p-3 pr-6">
         <div className="flex items-center flex-shrink-0 text-white sm:mr-12">
           <img className="h-20 mr-4" src={logo} alt="a white outline of a Carolina Reaper pepper in the shape of the race course with a green stem" />
-          <span className="text-xl md:text-2xl italic tracking-tight">CAROLINA REAPER<br /> ROAD RACE</span>
+          <span className="text-xl font-logo md:text-2xl italic tracking-tight">CAROLINA REAPER<br /> ROAD RACE</span>
         </div>
         <div className="block lg:hidden">
           <button 
@@ -36,14 +37,14 @@ const Header = () => {
               className={linkStyle}
               onClick={() => setIsNavOpen(!isNavOpen)}
             >
-              MORE INFO
+              RACE INFO
             </NavLink>
             <NavLink 
               to="/travel"
               className={linkStyle}
               onClick={() => setIsNavOpen(!isNavOpen)}
             >
-              TRAVEL & PARKING
+              GETTING THERE
             </NavLink>
             <NavLink 
               to="/results"
@@ -67,9 +68,7 @@ const Header = () => {
               CONTACT
             </NavLink>
           </div>
-          <div>
-            <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">REGISTER NOW</a>
-          </div>
+          <CtaButton />
         </div>
       </nav>
     </header>
