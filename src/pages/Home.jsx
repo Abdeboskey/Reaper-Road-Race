cb435b11b99be088c58edfc9b5970c6420c99606
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-27 13:49:01 -0600

    Apply custom markup components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,7 @@
+import H1 from 'components/markup/H1'
+import H2 from 'components/markup/H2'
+import P from 'components/markup/P'
+import Main from 'components/markup/Main'
 import CtaButton from 'components/CtaButton'
 import fullLogo from 'assets/color_logo_full.svg'
 import heroBG from 'assets/skaters.jpg'
@@ -5,22 +9,20 @@ import heroBG from 'assets/skaters.jpg'
 const Home = () => {
 
   return (
-    <main className="">
+    <div>
       <div className="relative w-screen text-white">
-        <img className="w-100" src={heroBG} alt="Silhouettes of an inline speed skate"/>
-        <img className="absolute inset-x-1/2 -translate-x-1/2 bottom-5 w-11/12 md:w-1/2" src={fullLogo} alt="Carolina Reaper Road Race Logo" />
+        <img className="w-screen" src={heroBG} alt="Inline skaters standing next to each other on a road"/>
+        <img className="absolute inset-x-1/2 -translate-x-1/2 inset-y-1/2 -translate-y-1/2 w-3/5 md:w-1/2" src={fullLogo} alt="Carolina Reaper Road Race Logo" />
       </div>
-      <div className="md:w-2/3 mx-auto py-10 text-white">
-        <h2 className="text-5xl tracking-wide text-center p-4 font-heading">
+      <Main>
+        <H1>
           The first skating race in Greenville, SC
-        </h2>
-        <p className="font-body text-4xl text-center p-4">
+        </H1>
+        <H2>
           Saturday, June 10th, 2023
-        </p>
-        <div className="flex justify-center align-center font-heading p-4">
-          <CtaButton />
-        </div>
-        <p className="font-body text-xl p-6">
+        </H2>
+        <CtaButton />
+        <P>
           Once upon a time Mark said he wasn't hungry for pancakes and he regretted that decision later.
           Once upon a time Mark said he wasn't hungry for pancakes and he regretted that decision later.
           Once upon a time Mark said he wasn't hungry for pancakes and he regretted that decision later.
@@ -29,9 +31,9 @@ const Home = () => {
           Once upon a time Mark said he wasn't hungry for pancakes and he regretted that decision later.
           Once upon a time Mark said he wasn't hungry for pancakes and he regretted that decision later.
           Once upon a time Mark said he wasn't hungry for pancakes and he regretted that decision later.
-        </p>
-      </div>
-    </main>
+        </P>
+      </Main>
+    </div>
   )
 }
 
