66eccd6117fe824da2623f9d0f0b95ee1192a2ce
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-04-05 15:03:44 -0600

    Center text and update styling

--- src/components/CtaButton.jsx
+++ src/components/CtaButton.jsx
@@ -1,8 +1,8 @@
 const CtaButton = ({ dark }) => {
-  const style = `inline-block text-2xl px-4 py-2 my-2 leading-none border rounded ${dark ? 'text-black border-black drop-shadow-md hover:bg-red-500 hover:text-gray-100' : 'text-gray-100 border-gray-100 hover:bg-gray-100 hover:text-red-500'} font-heading -skew-x-6 hover:border-transparent`
+  const style = `inline-block text-2xl px-4 py-2 my-2 leading-none border rounded ${dark ? 'text-black border-black shadow-md hover:bg-red-500 hover:text-gray-100' : 'text-gray-100 border-gray-100 hover:bg-gray-100 hover:text-red-500'} font-heading -skew-x-6 hover:border-transparent`
 
   return (
-    <div>
+    <div className="text-center">
       <a className={style} href="https://www.bikereg.com" target="_blank">
         REGISTER NOW
       </a>
