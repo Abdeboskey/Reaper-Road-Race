00e966ecf46da3d736dc1ce5fd7ebbc72ceaf107
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-04-05 15:02:18 -0600

    Update descriptions distances and styling

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -1,29 +1,29 @@
 import CategoryCard from 'components/CategoryCard'
 
 const CategoryContainer = () => {
-  const style = "md:flex bg-gradient-to-r from-red-400 to-red-700 shadow-lg rounded-xl p-2 my-4"
+  const style = "relative md:flex shadow-lg rounded-xl p-2 my-4"
   
   return (
     <div className={style}>
       <CategoryCard
         name="11.5 K"
-        description="See how fast you can complete a single lap of this spicy course."
+        description="Cheer on the Moreathon and Relay starts, then try to chase them all down in this single lap race."
         numLaps="1"
         distance="7.2"
         startTime="9:40"
       />
       <CategoryCard
         name="Moreathon"
-        description="Not for the faint of heart, take on the full-reaper in our 28.6 mile Moreathon."
+        description="When a Marathon just isn't enough anymore, we offer you the Moreathon."
         numLaps="4"
-        distance="28.6"
+        distance="28.8"
         startTime="9:30"
       />
       <CategoryCard
         name="Moreathon Relay"
         description="Team up with friends to complete the Moreathon as a 2-4 person relay."
         numLaps="4"
-        distance="28.6"
+        distance="28.8"
         startTime="9:30"
       />
     </div>
