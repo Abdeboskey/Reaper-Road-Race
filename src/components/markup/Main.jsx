918fc4a47d014348a5cc48392f605dcd34638c33
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-04-07 15:02:18 -0600

    Increase bottom padding

--- src/components/markup/Main.jsx
+++ src/components/markup/Main.jsx
@@ -1,5 +1,5 @@
 const Main = ({ children }) => {
-  const style = "text-white font-body md:w-2/3 md:text-xl lg:w-3/5 mx-auto p-4 pb-[450px]"
+  const style = "text-white font-body md:w-2/3 md:text-xl lg:w-3/5 mx-auto p-4 pb-[470px]"
 
   return (
     <main className={style}>
