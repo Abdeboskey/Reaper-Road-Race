5e99b4f5b3a7773f0bca5cc464f7a55ca67a611f
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-04-13 16:17:17 -0600

    Update getting there info issues #48 & #49

--- src/pages/GettingThere.jsx
+++ src/pages/GettingThere.jsx
@@ -2,8 +2,12 @@ import Main from 'components/markup/Main'
 import Section from 'components/markup/Section'
 import H1 from 'components/markup/H1'
 import H2 from 'components/markup/H2'
+import H3 from 'components/markup/H3'
 import P from 'components/markup/P'
 import AddressLink from 'components/AddressLink'
+import SwampRabbitSocial from 'components/SwampRabbitSocial'
+
+import siteMap from 'assets/site_map.png'
 
 const GettingThere = () => {
 
@@ -12,32 +16,45 @@ const GettingThere = () => {
       <Section>
         <H1>Getting There</H1>
         <H2>Event Location</H2>
+        <img
+          className="w-100 md:w-3/5 mx-auto my-4"
+          src={siteMap}
+          alt="Map of start/finish/relay area of the race course"
+        />
         <AddressLink />
         <P>
           Use the above address to navigate to the event location. Parking,
-          start/finish line, relay zone, on-site registration & packet pick-up,
-          and all race operations will be held at this address.
+          on-site registration & packet pick-up, and all race operations will
+          be held at this address.
         </P>
         <P>
-          On-site parking will be available. Note: some parking will be in grass
-          - plan accordingly for putting on skates. Surrounding roads will be
-          available for warming up, but please keep an eye out for traffic.
+          On-site parking will be available. Note: some parking will be in the
+          grass, plan accordingly for putting on skates. Surrounding roads will
+          be available for warming up, but please keep an eye out for traffic.
         </P>
       </Section>
       <Section>
         <H2>Staying in Greenville</H2>
+        <H3>
+          Hotel Suggestions
+        </H3>
         <P center>
-          Hotel Deals:
-        </P>
-        <P center>
-          // INSERT HOTEL INFORMATION //
+          <a
+            className="text-orange-500 underline font-bold"
+            href="https://www.woodspring.com/extended-stay-hotels/locations/SC599?bchan=GOO&utm_source=GoogleHPA&utm_medium=referral&utm_campaign=VPTEXC&from=2023-03-14&to=1%2520days&iata=05631194&gal&mc=HAGOXXWS&meta=PMFGPADWSRACK_SC599_mapresults_US_1_desktop_2023-03-14_default___organic&gmp=MetaOrganic#rates"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            WoodSpring Suites{" "} 
+          </a>
+          - Starting at $[SomeNum].95 per night.
         </P>
       </Section>
       <Section>
         <H2>Flying to Greenville</H2>
         <P>
           Fly in to the Greenville-Spartanburg International (GSP) airport. The
-          airport is approximately X from the local hotels.
+          airport is approximately 9 miles from WoodSpring Suites.
         </P>
       </Section>
       <Section>
@@ -48,10 +65,7 @@ const GettingThere = () => {
         <P center>
           // INSERT LOCAL PARTNERS //
         </P>
-        <P center>
-          Stay in town on Sunday to join us for a fun social skate on the scenic
-          Swamp Rabbit Trail!
-        </P>
+        <SwampRabbitSocial />
       </Section>
     </Main>
   )
