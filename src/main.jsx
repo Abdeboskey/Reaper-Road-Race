6d46030e0c5ecc5aa5759a6bfb44e94d56d1e85e
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-04-14 17:39:21 -0600

    Install createbrowserrouter issue #34

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from 'components/App'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom'
+
+import Layout from 'components/Layout'
+import Home from 'pages/Home'
+import RaceInfo from 'pages/RaceInfo'
+import GettingThere from 'pages/GettingThere'
+import Sponsors from 'pages/Sponsors'
+import Results from 'pages/Results'
+import Contact from 'pages/Contact'
+
 import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="race-info" element={<RaceInfo />} />
+      <Route path="travel" element={<GettingThere />} />
+      <Route path="results" element={<Results />} />
+      <Route path="sponsors" element={<Sponsors />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
