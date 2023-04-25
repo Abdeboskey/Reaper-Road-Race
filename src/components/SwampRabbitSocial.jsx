223cd11951f97ecee12b3e30ce4f67f6b5626fd7
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-04-25 12:55:32 -0600

    Add padding x issue #88

--- src/components/SwampRabbitSocial.jsx
+++ src/components/SwampRabbitSocial.jsx
@@ -2,7 +2,7 @@ import H3 from 'components/markup/H3'
 import P from 'components/markup/P'
 
 const SwampRabbitSocial = () => {
-  const style = "bg-gradient-to-b from-orange-300 from-10% shadow-md rounded-xl md:w-2bg-gradient-to-b from-orange-300 from-10% shadow-md rounded-xl md:w-2/3 my-3 mx-auto py-6"
+  const style = "bg-gradient-to-b from-orange-300 from-10% shadow-md rounded-xl md:w-2bg-gradient-to-b from-orange-300 from-10% shadow-md rounded-xl md:w-2/3 my-3 mx-auto py-6 px-4"
   const linkStyle = "text-orange-500 underline"
 
   return (
