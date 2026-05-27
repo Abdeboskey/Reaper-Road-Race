2bfd6c743ad329650777d9782409c63d6acd95a6
whereareweskating <129459031+whereareweskating@users.noreply.github.com>
2026-05-27 16:59:42 -0600

    Update SwampRabbitSocial.jsx

--- src/components/SwampRabbitSocial.jsx
+++ src/components/SwampRabbitSocial.jsx
@@ -21,9 +21,9 @@ const SwampRabbitSocial = () => {
         </a>
         {" "}starting at 9:45 am, and will roll out at 10:00 am for a fun
         out-and-back skate. A2A time rules are in effect, and we will leave
-        on time after a group photo. Mileage will range from 5-20 depending on
-        participants' interests. We will meet back at the start afterwards for
-        lunch and some spicy conversation.
+        on time after a group photo. Mileage will range from 5-20 miles 
+        depending on participants' interests. We will meet back at the start 
+        afterwards for lunch and some spicy conversation.
         HELMETS REQUIRED.
       </P>
     </div>
